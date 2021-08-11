afa68e3d120f45bcdd934cf5840cda909d7739f5
Saurabh Kumar <sauravgautam884@gmail.com>
2021-08-11 12:31:29 +0530

    change bg and typing animation

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,16 @@
-import React from 'react'
+import React,{useEffect} from 'react'
+import Typewriter from 'typewriter-effect/dist/core';
 
 const Home = ({isAtTop}) => {
+
+    useEffect(() => {
+        new Typewriter('#typewriter', {
+            strings: ['Javascript Developer.', 'C++ Programmer.'],
+            autoStart: true,
+            loop: true
+        })          
+    },[])
+
     return (
         <section id='home' className='py-3 py-md-5'>
             <div className='container py-3 py-md-5'>
@@ -10,12 +20,10 @@ const Home = ({isAtTop}) => {
                             <small>Hello, I'm</small>
                             <br/>
                             Saurabh Kumar.
+                            <br/>
+                            <small className="profession">And I'm a <span id="typewriter"></span></small>
                         </h3>
-                        <div className="d-flex flex-column flex-md-row">
-                            <h6 className='pt-1 pt-md-3 mb-0'><span>|</span> JAVASCRIPT DEVELOPER <span>|</span></h6>
-                            <h6 className='pt-1 pt-md-3 mb-0'>C++ PROGRAMMER <span>|</span></h6>
-                        </div>
-                        <a href='#about' type="button" className="btn btn-lg btn-dark mt-5 me-md-3 px-4 px-md-4 py-3 py-md-3 fw-bold">KNOW MORE <i className="fas fa-caret-down"></i></a>
+                        <a href='#about' type="button" className="btn btn-lg mt-5 me-md-3 px-4 px-md-4 py-3 py-md-3 fw-bold">KNOW MORE <i className="fas fa-caret-down"></i></a>
                     </div>
                 </div>
             </div> 
